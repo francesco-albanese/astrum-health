79d59ba24323ab00cf1272da116272e0f101c6e1
francesco-albanese <albanesefrancesco.af@gmail.com>
2016-04-15 11:25:55 +0100

    Gruntfile fully configured, tested and working

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,115 +1,153 @@
 module.exports = function(grunt) {
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
 
-    jade: {
-      compile: {
-        options: {
-          data: {
-            debug: false
-          }
-        },
-        files: {
-          ['app', 'dist'] : 'app/jade/*.jade'
-        }
-      },
-    }, // end jade
+    require('load-grunt-tasks')(grunt); // required to load cssnano and sass
 
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true
-        },
-      },
-      files: ['Gruntfile.js', 'app/js/*.js'],
-    }, // end jshint
+    // Project configuration
+    grunt.initConfig({
 
-    uglify: {
-      my_target: {
-        options: {
-          sourceMap: true,
-          sourceMapName: 'app/js/jsSourceMap.map',
-          beautify: {
-            width: 80,
-            beautify: true
-          }
-        },
-        files: {
-          'dist/bundle.min.js' : ['app/index.js']
-        }
-      },
-    }, // end uglify
+        jade: {
+            build: {
+                files: [ {
+                    cwd: './app/jade/',
+                    src: '*.jade',
+                    dest: 'dist/',
+                    expand: true,
+                    ext: '.html'
+                }, {
+                    cwd: './app/jade/',
+                    src: '*.jade',
+                    dest: './app',
+                    expand: true,
+                    ext: '.html'
+                } ]
+            },
+        }, // end jade
 
-    postcss: {
-      options: {
-        map: {
-          inline: false,
-          annotation: 'app/css'
-        },
-        processors: [
-          require('pixrem')(),
-          require('autoprefixer')({browsers: ['last 10 versions']}),
-          require('cssnano')()
-        ]
-      },
-      dist: {
-        src: 'app/css/*.css'
-      }
-    }, // end postcss
+        sass: {
+            build: {
+                options: {
+                    style: 'expanded'
+                },
+                files: [ {
+                    cwd: './app/scss/',
+                    src: 'main.scss',
+                    expand: true,
+                    dest: './app/css',
+                    ext: '.css'
+                } ]
+            },
+        }, // end sass
 
-    concat: {
-      options: {
-        separator: ';\n',
-        banner: 'Begin JS files concatenation',
-        footer: 'end JS files concatenation',
-        sourceMap: true,
-      },
-      dist: {
-        src: ['app/js/index.js', 'app/js/index2.js'],
-        dest: ['dist/js/bundle.js', 'app/js/bundle.js']
-      },
-    }, // end concat
+        postcss: {
+            build: {
+                options: {
+                    map: {
+                        inline: false,
+                        annotation: 'app/css/'
+                    },
+                    processors: [
+                        require('autoprefixer')({browsers: 'last 10 versions'}),
+                        require('cssnano')()
+                    ]
+                },
+                files: {
+                    'app/css/main.min.css' : 'app/css/main.css',
+                    'dist/css/main.min.css' : 'app/css/main.css'
+                }
+            },
+        }, // end postcss
 
-    imagemin: {
-      options: {
-        optimizationLevel: 5,
-        progressive: true,
-        interlaced: true,
-      },
-      files: {
-        src: ['app/images/**/*.{png,jpg,gif,svg}'],
-        dest: 'dist/images'
-      },
-    }, // end imagemin
+        jshint: {
+            build: {
+                options: {
+                    reporter: require('jshint-stylish')
+                },
+                files: {
+                    src: ['Gruntfile.js','./app/js/*.js', 'dist/js/*.js']
+                }
+            }
+        }, // end jshint
 
-    browserSync: {
-      dev: {
-        bsFiles: {
-          src: [
-            'app/css/*.css',
-            'app/*.html'
-          ]
-        },
-        options: {
-          watchTask: true,
-          server: './app'
-        },
-      },
-    }, // end browserSync
+        uglify: {
+            build: {
+                options: {
+                    mangle: false
+                },
+                files: [ {
+                    cwd: './app/js/',
+                    src: '*.js',
+                    dest: 'dist/js',
+                    expand: true,
+                    ext: '.min.js'
+                } ]
+            }
+        }, // end uglify
 
-  });
-  // Tasks.
-  grunt.loadNpmTasks('grunt-contrib-jade');
-  grunt.loadNpmTasks('grunt-postcss');
-  grunt.loadNpmTasks('grunt-browser-sync');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+        imagemin: {
+            build: {
+                options: {
+                    optimizationLevel: 5,
+                    progressive: true,
+                    interlaced: true,
+                },
+                files: [ {
+                    cwd: './app/images/',
+                    src: '**/*.{png,jpg,gif,svg}',
+                    dest: 'dist/images',
+                    expand: true,
+                } ]
+            }
+        }, // end imagemin
+
+        browserSync: {
+            build: {
+                bsFiles: {
+                    src: [
+                        'app/*.html',
+                        'app/css/main.css',
+                        'app/js/**/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './app'
+                }
+            }
+        }, // end browserSync
+
+        watch: {
+            jade: {
+                files: 'app/jade/*.jade',
+                tasks: ['jade']
+            },
+            stylesheet: {
+                files: 'app/scss/main.scss',
+                tasks: ['sass', 'postcss']
+            },
+            scripts: {
+                files: 'app/js/*.js',
+                tasks: ['jshint', 'uglify']
+            },
+            images: {
+                files: ['app/images/**/*.{png,jpg,gif,svg}'],
+                tasks: ['imagemin:build']
+            }
+
+        }, // end watch
+
+
+    });
+
+    // enable plugins with call
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // register tasks
+    grunt.registerTask('default', ['browserSync', 'watch']);
+
 };
