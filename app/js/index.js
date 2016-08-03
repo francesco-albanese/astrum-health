5e8f89f4e14286f6b2b449649c73c3f68086d3d2
francesco-albanese <albanesefrancesco.af@gmail.com>
2016-08-03 20:29:54 +0200

    2103.08.2016 - Completed development of testimonials-section and footer-top section

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,43 @@
 (function() {
-    console.log('test');
+  var googleMap = {
+
+    location: {
+      title: 'Redcliffe, Bristol, UK',
+      location: {lat: 51.4484272, lng: -2.5874823},
+    },
+
+    mapOptions: {
+      center: {lat: 51.454513, lng: -2.58791},
+      zoom: 15,
+      mapTypeControl: false,
+      scrollwheel: false
+    },
+    initMap: function() {
+      var map = new google.maps.Map(document.querySelector('.footer__top-map'), this.mapOptions);
+      this.map = map;
+    },
+
+    appendMarkers: function (map) {
+      var bounds = new google.maps.LatLngBounds();
+      var position = this.location.location;
+      var title    = this.location.title;
+      var marker = new google.maps.Marker({
+        position: position,
+        title: title,
+        animation: google.maps.Animation.DROP
+      });
+      // bounds.extend(marker.position);
+      marker.setMap(map);
+      // map.fitBounds(bounds);
+    },
+
+    init: function() {
+      var self = this;
+      this.initMap();
+      this.appendMarkers(self.map);
+    }
+  };
+  // this is required to make googleMAP.init available for google maps callback
+  window.googleMap = googleMap;
+  return googleMap.init();
 })();
