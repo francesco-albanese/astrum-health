64cfd075439e16bf7660608d7d14b64b3281ac29
francesco-albanese <albanesefrancesco.af@gmail.com>
2016-11-12 16:58:45 +0100

    12.11.2016 - Added animations to solutions section

--- app/js/es6/navbar.es6
+++ app/js/es6/navbar.es6
@@ -19,17 +19,55 @@
     },
 
     cacheDom() {
+      this.window = $(window);
       this.burgerMenu = $('.burger-menu');
-      this.offScreenMenu = $('.offscreen-menu');
+      this.offScreenMenu = this.burgerMenu.parents('body').find('.offscreen-menu');
       this.closeMenu = this.offScreenMenu.children('.offscreen-menu__close');
       this.body = this.burgerMenu.parents('body');
       this.offScreenLinks = this.offScreenMenu.find('a[href="#"]');
+      this.header = this.burgerMenu.parent();
+      this.solutionsSection = this.body.find('.solutions-section');
     },
 
     bindEvents() {
       this.burgerMenu.on('click', this.openOffScreenMenu.bind(this));
       this.closeMenu.on('click', this.closeOffScreenMenu.bind(this));
       this.offScreenLinks.on('click', this.closeOffScreenMenu.bind(this));
+      this.window.on('scroll', this.toggleHeader.bind(this));
+      this.window.on('scroll', this.animateSolutionsSection.bind(this));
+    },
+
+    isElementVisible(element) {
+      let visible = false;
+      let bottom_of_object = (element.offset().top + element.outerHeight()) - (element.outerHeight() / 2);
+      let bottom_of_window = navbar.window.scrollTop() + navbar.window.height();
+      /* If the object is completely visible in the window, fade it it */
+      if( bottom_of_window > bottom_of_object ){
+        visible = true;
+      }
+      return visible;
+    },
+
+    animateSolutionsSection() {
+      const articles = this.solutionsSection.find('article');
+      articles.each(function(i, elem) {
+        let visible = navbar.isElementVisible($(elem));
+        if (visible && (i % 2 !== 0)) {
+          $(elem).addClass('fadeInRightBig');
+        } else if(visible && (i % 2 === 0)) {
+          $(elem).addClass('fadeInLeftBig');
+        }
+      });
+    },
+
+    toggleHeader() {
+      let windowScroll = this.window.scrollTop();
+      const headerHeight = this.header.outerHeight();
+      if (windowScroll > headerHeight) {
+        this.header.addClass('header--fixed-top');
+      } else {
+        this.header.removeClass('header--fixed-top');
+      }
     },
 
     openOffScreenMenu() {
--- app/js/es6/navbar.js
+++ app/js/es6/navbar.js
@@ -19,16 +19,51 @@
       });
     },
     cacheDom: function cacheDom() {
+      this.window = $(window);
       this.burgerMenu = $('.burger-menu');
-      this.offScreenMenu = $('.offscreen-menu');
+      this.offScreenMenu = this.burgerMenu.parents('body').find('.offscreen-menu');
       this.closeMenu = this.offScreenMenu.children('.offscreen-menu__close');
       this.body = this.burgerMenu.parents('body');
       this.offScreenLinks = this.offScreenMenu.find('a[href="#"]');
+      this.header = this.burgerMenu.parent();
+      this.solutionsSection = this.body.find('.solutions-section');
     },
     bindEvents: function bindEvents() {
       this.burgerMenu.on('click', this.openOffScreenMenu.bind(this));
       this.closeMenu.on('click', this.closeOffScreenMenu.bind(this));
       this.offScreenLinks.on('click', this.closeOffScreenMenu.bind(this));
+      this.window.on('scroll', this.toggleHeader.bind(this));
+      this.window.on('scroll', this.animateSolutionsSection.bind(this));
+    },
+    isElementVisible: function isElementVisible(element) {
+      var visible = false;
+      var bottom_of_object = element.offset().top + element.outerHeight() - element.outerHeight() / 2;
+      var bottom_of_window = navbar.window.scrollTop() + navbar.window.height();
+      /* If the object is completely visible in the window, fade it it */
+      if (bottom_of_window > bottom_of_object) {
+        visible = true;
+      }
+      return visible;
+    },
+    animateSolutionsSection: function animateSolutionsSection() {
+      var articles = this.solutionsSection.find('article');
+      articles.each(function (i, elem) {
+        var visible = navbar.isElementVisible($(elem));
+        if (visible && i % 2 !== 0) {
+          $(elem).addClass('fadeInRightBig');
+        } else if (visible && i % 2 === 0) {
+          $(elem).addClass('fadeInLeftBig');
+        }
+      });
+    },
+    toggleHeader: function toggleHeader() {
+      var windowScroll = this.window.scrollTop();
+      var headerHeight = this.header.outerHeight();
+      if (windowScroll > headerHeight) {
+        this.header.addClass('header--fixed-top');
+      } else {
+        this.header.removeClass('header--fixed-top');
+      }
     },
     openOffScreenMenu: function openOffScreenMenu() {
       this.body.addClass('no-scroll');
