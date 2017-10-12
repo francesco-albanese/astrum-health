7848cf8c5d178c5e6a945663a791da1a41120da9
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2017-10-13 00:47:41 +0200

    Fixed a typo in grunt clean task

--- grunt/clean.js
+++ grunt/clean.js
@@ -1,4 +1,4 @@
 module.exports = {
     cleanDist: ['dist'],
-    cleanJS: ['dist/js/**.js', '!dist/js/bundle.js', '!dist/js/displaymode.min.js', '!dist/js/tables.min.js', '!./dist/js/loadvideo.min.js']
+    cleanJS: ['dist/js/**.js', '!dist/js/bundle.js', '!dist/js/displaymode.min.js', '!dist/js/tables.min.js', '!dist/js/loadvideo.min.js']
 }
